a60c14561d7f709d4660636093baf5e39ba3ecf7
Nrsken <erikabrah34@gmail.com>
2026-07-05 20:16:21 +0200

    Add cleaner backdrop util

---
+++ Core/Backdrop.lua
@@ -0,0 +1,86 @@
+---@class NRSKNUI
+local NRSKNUI = select(2, ...)
+
+local Mixin = Mixin
+local CreateFrame = CreateFrame
+local pairs = pairs
+local table_insert = table.insert
+
+---@class PublicBackdropMixin : Frame
+---@field BackDropBorders table
+local PublicBackdropMixin = {}
+
+---Set the color of the background
+---@param r number
+---@param g number
+---@param b number
+---@param a? number
+function PublicBackdropMixin:SetBackgroundColor(r, g, b, a)
+	if self.backdropBackground then
+		self.backdropBackground:SetColorTexture(r, g, b, a)
+	end
+end
+
+---Set the color of the backdrops borders
+---@param r number
+---@param g number
+---@param b number
+---@param a? number
+function PublicBackdropMixin:SetBorderColor(r, g, b, a)
+	if self.BackDropBorders then
+		for _, edge in pairs(self.BackDropBorders) do
+			edge:SetColorTexture(r, g, b, a)
+		end
+	end
+end
+
+local CreateTextureMixin = CreateFrame("Frame").CreateTexture
+
+---@param frame Frame
+function NRSKNUI:CreateBackdrop(frame)
+	---@cast frame Frame & PublicBackdropMixin
+	Mixin(frame, PublicBackdropMixin)
+
+	frame.BackDropBorders = {}
+
+	local BG = CreateTextureMixin(frame, nil, "BACKGROUND")
+	--BG:SetAllPoints(frame)
+	BG:SetPoint("TOPLEFT", frame, "TOPLEFT",1, -1)
+	BG:SetPoint("BOTTOMRIGHT", frame, "BOTTOMRIGHT",-1, 1)
+	frame.backdropBackground = BG
+	NRSKNUI:PixelPerfect(BG)
+
+	local LB = CreateTextureMixin(frame, nil, "BORDER")
+	LB:SetPoint("TOPLEFT", frame, 1, -1)
+	LB:SetPoint("BOTTOMLEFT", frame, 1, 1)
+	table_insert(frame.BackDropBorders, LB)
+	NRSKNUI:PixelPerfect(LB)
+
+	local TB = CreateTextureMixin(frame, nil, "BORDER")
+	TB:SetPoint("TOPLEFT", frame, 1, -1)
+	TB:SetPoint("TOPRIGHT", frame, -1, -1)
+	table_insert(frame.BackDropBorders, TB)
+	NRSKNUI:PixelPerfect(TB)
+
+	local RB = CreateTextureMixin(frame, nil, "BORDER")
+	RB:SetPoint("TOPRIGHT", frame, -1, -1)
+	RB:SetPoint("BOTTOMRIGHT", frame, -1, 1)
+	table_insert(frame.BackDropBorders, RB)
+	NRSKNUI:PixelPerfect(RB)
+
+	local BB = CreateTextureMixin(frame, nil, "BORDER")
+	BB:SetPoint("BOTTOMLEFT", frame, 1, 1)
+	BB:SetPoint("BOTTOMRIGHT", frame, -1, 1)
+	table_insert(frame.BackDropBorders, BB)
+	NRSKNUI:PixelPerfect(BB)
+
+	frame:SetBackgroundColor(0, 0, 0, 0.8)
+	frame:SetBorderColor(0, 0, 0, 1)
+end
+
+---Check if we already added a backdrop to the frame
+---@param frame Frame
+---@return boolean
+function NRSKNUI:BackdropExists(frame)
+	return not not frame.BackDropBorders
+end
