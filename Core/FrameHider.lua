bb776e8b569686f7dd68d436f9ff7b709e99916f
Nrsken <erikabrah34@gmail.com>
2026-07-07 02:10:55 +0200

    Add global helper that Iterates over a frame's child frames and hides each Texture region.

--- Core/FrameHider.lua
+++ Core/FrameHider.lua
@@ -7,9 +7,10 @@ local CreateFrame = CreateFrame
 local type = type
 local select = select
 local pcall = pcall
+local ipairs = ipairs
 local _G = _G
 
--- hidden dummy frame we anchor stuff we want to hide to
+-- Hidden dummy frame we anchor stuff we want to hide to
 local hidden = CreateFrame('Frame')
 hidden:Hide()
 
@@ -21,7 +22,7 @@ function NRSKNUI:Hide(object, ...)
     end
 
     if ... then
-        -- iterate through arguments, they're children referenced by key
+        -- Iterate through arguments, they're children referenced by key
         for index = 1, select('#', ...) do
             object = object[select(index, ...)]
         end
@@ -29,7 +30,7 @@ function NRSKNUI:Hide(object, ...)
 
     if object then
         if object.HideBase then
-            object:HideBase(true) -- edit mode adds this fallback when it overrides Hide
+            object:HideBase(true) -- Edit mode adds this fallback when it overrides Hide
         else
             object:Hide(true)
         end
@@ -40,11 +41,11 @@ function NRSKNUI:Hide(object, ...)
 
         if object.UnregisterAllEvents then
             object:UnregisterAllEvents()
-            object:SetAttribute('statehidden', true) -- useful for hiding secure template based objects
+            object:SetAttribute('statehidden', true) -- Useful for hiding secure template based objects
         end
 
         if object.SetUserPlaced then
-            -- useful for hiding blizzard objects that respect user placement
+            -- Useful for hiding blizzard objects that respect user placement
             pcall(object.SetUserPlaced, object, true)
             pcall(object.SetDontSavePosition, object, true)
         end
@@ -55,3 +56,23 @@ end
 
 -- Expose frame so it can be accessed
 NRSKNUI.HiddenFrame = hidden
+
+---Iterates over a frame's child frames and hides each Texture region.
+---@param frame Frame | string
+function NRSKNUI:HideTextures(frame, ...)
+    if type(frame) == 'string' then
+        frame = _G[frame]
+    end
+
+    for _, region in ipairs({ frame:GetRegions() }) do
+        if ... then
+            if region:IsObjectType("Texture") and region:GetAtlas() == ... then
+                region:Hide()
+            end
+        else
+            if region:IsObjectType("Texture") then
+                region:Hide()
+            end
+        end
+    end
+end
